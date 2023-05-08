7a6abd6e69adcbfae4dadc74344f96ac13a57762
Cuong Pham <pvcuongkg123@gmail.com>
2023-05-08 17:10:09 +0700

    init project

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import { Container } from 'react-bootstrap';
+import styles from './Home.module.scss';
+
+const Home = () => {
+    return (
+        <Container>
+            <h1>HOME PAGE</h1>
+        </Container>
+    );
+};
+
+export default Home;
